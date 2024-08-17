6e7312e167840fd2d0de38d8c44ddfb0caf46529
Mohit Pardhi <34490195+mpardhi11@users.noreply.github.com>
2024-08-17 12:09:30 +0530

    simple nav bar

---
+++ create-navbar/src/App.jsx
@@ -0,0 +1,22 @@
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NoMatch from "./components/NoMatch";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
